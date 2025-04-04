97234181328c7336ee931cd8be2ea617fc52d104
CL <leungc22@gmail.com>
2025-04-03 22:51:37 -0400

    added form event

--- scripts/script.js
+++ scripts/script.js
@@ -3,11 +3,11 @@ const button = document.getElementById('mouseButton');
 const message = document.getElementById('message');
 
 button.addEventListener('mouseover', () => {
-    message.textContent = "Button is hovered!";
+    message.textContent = "(h)over";
 });
 
 button.addEventListener('mouseout', () => {
-    message.textContent = "Hover ended.";
+    message.textContent = "over";
 });
 
 // KEYBOARD EVENTS
@@ -15,11 +15,24 @@ const keyInput = document.getElementById('keyInput');
 const keyCodeMessage = document.getElementById('keyCodeMessage');
 
 keyInput.addEventListener('keydown', (event) => {
+    event.preventDefault();
+    keyInput.value = event.key;
     keyCodeMessage.textContent = `key pressed: ${event.key}`;
 });
 
 // FORM EVENTS
+const form = document.getElementById('myForm');
+const formMessageInput = document.getElementById('formInput');
+const formMessageOutput = document.getElementById('formOutput');
 
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    const savedMessage = formMessageInput.value;
+    localStorage.setItem('formMessage', savedMessage);
+    formMessageOutput.textContent = `"${savedMessage}" is saved in localStorage`;
+    formMessageInput.value = '';
+});
 
 // FOCUS AND BLUR EVENTS
 
